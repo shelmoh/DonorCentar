1ca5118be423dc2ca0ec02c09974605aa52e4ffe
Selma Mujan <selma.mujan@edu.fit.ba>
2021-08-14 18:20:48 +0200

    add Xamarin

---
+++ DonorCentar.Mobile/DonorCentar.Mobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using DonorCentar.Mobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace DonorCentar.Mobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
