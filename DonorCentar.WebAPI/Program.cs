1e7eda1e60bdeb11f74c5f12a14bf311c07bb6f1
Selma Mujan <selma.mujan@edu.fit.ba>
2021-09-12 02:19:27 +0200

    Dodan database seeder

--- DonorCentar.WebAPI/Program.cs
+++ DonorCentar.WebAPI/Program.cs
@@ -1,5 +1,7 @@
+using DonorCentar.WebAPI.Database;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,7 +15,15 @@ namespace DonorCentar.WebAPI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var ctx = scope.ServiceProvider.GetService<BazaPodataka>();
+                DatabaseInitializer.Initialize(ctx);
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
